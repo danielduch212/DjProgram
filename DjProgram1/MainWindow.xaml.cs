047cded4544531eb59381bb9c80728fec5bd93c3
danielduch212 <danielduchnowicz1@gmail.com>
2023-07-03 23:56:08 +0200

    Improvements

--- DjProgram1/MainWindow.xaml.cs
+++ DjProgram1/MainWindow.xaml.cs
@@ -14,22 +14,25 @@ using DjProgram1.Data;
 using NAudio.Wave;
 
 
+
 using Path = System.IO.Path;
-
-
-
-
+using VisioForge.Libs.WindowsMediaLib;
 
 namespace DjProgram1
 {
     
     public partial class MainWindow : Window
     {
-        
+        DjProgram1.Services.MusicService musicService = new DjProgram1.Services.MusicService(); 
         List<DjProgram1.Data.AudioFile> audioFiles = new List<DjProgram1.Data.AudioFile>();
         DjProgram1.Data.AudioFile currentAudioFile1 = new DjProgram1.Data.AudioFile();
         DjProgram1.Data.AudioFile currentAudioFile2 = new DjProgram1.Data.AudioFile();
-
+        
+        private NAudio.Wave.AudioFileReader audioFileReader1;
+        private NAudio.Wave.AudioFileReader audioFileReader2;
+        private NAudio.Wave.WaveOut waveOut1;
+        private NAudio.Wave.WaveOut waveOut2;
+        private int lastDeck = 2;
 
         public MainWindow()
         {
@@ -51,14 +54,26 @@ namespace DjProgram1
 
         private void buttonUpload_Click(object sender, RoutedEventArgs e)
         {
+            int selectedIndex = songList.SelectedIndex;
             if (songList.SelectedItem != null)
             {
-                int selectedIndex = songList.SelectedIndex;
+                
                 if (selectedIndex >= 0 && selectedIndex < audioFiles.Count)
                 {
-                    currentAudioFile1 = audioFiles[selectedIndex];
-                    //tutaj robic sprawdzenie czy cos jest odpalone itd
-
+                    if(lastDeck == 2)
+                    {
+                        currentAudioFile1 = audioFiles[selectedIndex];
+                        songOnDeck1.Text = currentAudioFile1.FileName;
+                        bpmTextBox1.Text = "BPM: " + musicService.GetBpm(currentAudioFile1.FilePath).ToString();
+                        lastDeck = 1;
+                    }
+                    else
+                    {
+                        currentAudioFile2 = audioFiles[selectedIndex];
+                        songOnDeck2.Text = currentAudioFile2.FileName;
+                        bpmTextBox2.Text = "BPM: " + musicService.GetBpm(currentAudioFile2.FilePath).ToString();
+                        lastDeck = 2;
+                    }
                     
                 }
             }
@@ -87,72 +102,60 @@ namespace DjProgram1
             }
         }
 
-        private void createWaveform(Canvas canvas)
-        {
-            {
-                // Wczytaj piosenkę z pliku audio
-                string filePath = "ścieżka_do_pliku_audio";
-                NAudio.Wave.WaveStream waveStream = new NAudio.Wave.WaveFileReader(filePath);
-
-                // Przetwórz dane audio i uzyskaj wygląd waveform
-                int sampleRate = waveStream.WaveFormat.SampleRate;
-                byte[] audioData = new byte[waveStream.Length];
-                waveStream.Read(audioData, 0, (int)waveStream.Length);
-                float[] audioSamples = new float[audioData.Length / 2];
-                for (int i = 0; i < audioSamples.Length; i++)
-                {
-                    short sampleValue = BitConverter.ToInt16(audioData, i * 2);
-                    audioSamples[i] = sampleValue / 32768f;
-                }
-
-                // Wygeneruj wizualizację waveform
-                double canvasWidth = canvas.ActualWidth;
-                double canvasHeight = canvas.ActualHeight;
-                canvas.Children.Clear();
-                for (int i = 0; i < audioSamples.Length; i++)
-                {
-                    double x = canvasWidth * i / audioSamples.Length;
-                    double y = canvasHeight * (1 - (audioSamples[i] + 1) / 2);
-                    Line line = new Line
-                    {
-                        X1 = x,
-                        Y1 = canvasHeight / 2,
-                        X2 = x,
-                        Y2 = y,
-                        Stroke = Brushes.Black
-                    };
-                    canvas.Children.Add(line);
-                }
-
-                // Zamknij strumień audio
-                waveStream.Close();
-            }
-        }
+        
         private void playButton1_Click(object sender, RoutedEventArgs e)
         {
-            var waveOut = new NAudio.Wave.WaveOut();
-            var audioFileReader = new NAudio.Wave.AudioFileReader(currentAudioFile1.FilePath);
-            waveOut.Init(audioFileReader);
-            waveOut.Play();
+            if (waveOut1 == null)
+            {
+                //musicService.createWaveform(canvas2, currentAudioFile2.FilePath);
+
+                audioFileReader1 = new NAudio.Wave.AudioFileReader(currentAudioFile1.FilePath);
+                waveOut1 = new NAudio.Wave.WaveOut();
+                waveOut1.Init(audioFileReader1);
+                waveOut1.Play();
+            }
+            else if (waveOut1.PlaybackState == NAudio.Wave.PlaybackState.Paused)
+            {
+                waveOut1.Play();
+            }
         }
 
         private void playButton2_Click(object sender, RoutedEventArgs e)
         {
-            var waveOut = new NAudio.Wave.WaveOut();
-            var audioFileReader = new NAudio.Wave.AudioFileReader(currentAudioFile2.FilePath);
-            waveOut.Init(audioFileReader);
-            waveOut.Play();
+            if (waveOut2 == null)
+            {
+                //musicService.createWaveform(canvas2, currentAudioFile2.FilePath);
+
+                audioFileReader2 = new NAudio.Wave.AudioFileReader(currentAudioFile2.FilePath);
+                waveOut2 = new NAudio.Wave.WaveOut();
+                waveOut2.Init(audioFileReader2);
+                waveOut2.Play();
+            }
+            else if (waveOut1.PlaybackState == NAudio.Wave.PlaybackState.Paused)
+            {
+                waveOut2.Play();
+            }
         }
 
         private void pauseButton1_Click(object sender, RoutedEventArgs e)
         {
-
+            if (waveOut1 != null && waveOut1.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+            {
+                waveOut1.Pause();
+            }
         }
 
         private void pauseButton2_Click(object sender, RoutedEventArgs e)
         {
-
+            if (waveOut2 != null && waveOut2.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+            {
+                waveOut2.Stop();
+                waveOut2.Dispose();
+                
+            }
         }
+
+        
 
         //przeorganizowac kod - zastanowić się jak najlepiej ma on byc
         // 
--- DjProgram1/Services/MusicService.cs
+++ DjProgram1/Services/MusicService.cs
@@ -1,12 +1,94 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VisioForge.MediaFramework.NAudio.VisioForge;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using SoundTouch;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using NAudio.SoundTouch;
+
 
 namespace DjProgram1.Services
 {
     class MusicService
     {
+        public double GetBpm(string filePath)
+        {
+            using (var audioFileReader = new NAudio.Wave.AudioFileReader(filePath))
+            {
+                var soundTouch = new SoundTouch.BpmDetect(audioFileReader.WaveFormat.SampleRate, audioFileReader.WaveFormat.Channels);
+
+                int blockSize = 8192;
+                var buffer = new float[blockSize];
+                int samplesRead;
+
+                do
+                {
+                    samplesRead = audioFileReader.Read(buffer, 0, blockSize);
+                    soundTouch.InputSamples(buffer, samplesRead);
+                    
+                } while (samplesRead > 0);
+
+                
+                double bpm = soundTouch.GetBpm();
+
+                return bpm;
+            }
+        }
+
+        public void createWaveform(Canvas canvas, string filePath)
+        {
+            NAudio.Wave.WaveStream waveStream = new NAudio.Wave.WaveFileReader(filePath);
+            int sampleRate = waveStream.WaveFormat.SampleRate;
+
+            const int blockSize = 512; // Rozmiar bloku przetwarzania
+            int blockCount = (int)Math.Ceiling((double)waveStream.Length / blockSize);
+
+            // Inicjalizacja tablic przechowujących dane audio i wizualizację waveform
+            byte[] audioData = new byte[blockSize * blockCount];
+            float[] audioSamples = new float[audioData.Length / 2];
+            double canvasWidth = canvas.ActualWidth;
+            double canvasHeight = canvas.ActualHeight;
+            canvas.Children.Clear();
+
+            // Wczytanie i przetwarzanie danych audio blok po bloku
+            for (int blockIndex = 0; blockIndex < blockCount; blockIndex++)
+            {
+                int bytesRead = waveStream.Read(audioData, blockIndex * blockSize, blockSize);
+                int sampleCount = bytesRead / 2;
+
+                for (int i = 0; i < sampleCount; i++)
+                {
+                    short sampleValue = BitConverter.ToInt16(audioData, (blockIndex * blockSize) + (i * 2));
+                    audioSamples[(blockIndex * sampleCount) + i] = sampleValue / 32768f;
+                }
+
+                // Generowanie wizualizacji waveform
+                for (int i = 0; i < audioSamples.Length; i++)
+                {
+                    double x = canvasWidth * i / audioSamples.Length;
+                    double y = canvasHeight * (1 - (audioSamples[i] + 1) / 2);
+                    Line line = new Line
+                    {
+                        X1 = x,
+                        Y1 = canvasHeight / 2,
+                        X2 = x,
+                        Y2 = y,
+                        Stroke = Brushes.Black
+                    };
+                    canvas.Children.Add(line);
+                }
+            }
+
+            // Zamknięcie strumienia audio
+            waveStream.Close();
+        }
     }
 }
+
